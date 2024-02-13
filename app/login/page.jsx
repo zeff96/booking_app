c1b189811948f4f6f7c1f4056db8f866eac39413
zeff96 <zeffo1975@gmail.com>
2024-02-13 22:58:53 +0300

    Add login form

---
+++ app/login/page.jsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useFormStatus, useFormState } from "react-dom";
+import { authenticate } from "../lib/action";
+
+export default function Page() {
+  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
+
+  return (
+    <form action={dispatch}>
+      <input type="email" name="email" placeholder="Email" required />
+      <input type="password" name="password" placeholder="Password" required />
+      <div>{errorMessage && <p>{errorMessage}</p>}</div>
+      <LoginButton />
+    </form>
+  );
+}
