def5407f5529483b67fd1bcc8eb16f0500a3f067
zeff96 <zeffo1975@gmail.com>
2024-03-01 16:31:00 +0300

    Add styles for signup link

--- app/login/login.jsx
+++ app/login/login.jsx
@@ -2,6 +2,7 @@
 
 import { signIn } from "next-auth/react";
 import styles from "./login.module.css";
+import Link from "next/link";
 
 export default function LoginPage() {
   async function handleSubmit(event) {
@@ -17,21 +18,29 @@ export default function LoginPage() {
   }
   return (
     <form onSubmit={handleSubmit} className={styles.form}>
-      <input
-        type="email"
-        name="email"
-        placeholder="Email"
-        required
-        className={styles["form-control"]}
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        required
-        className={styles["form-control"]}
-      />
-      <input type="submit" value="Login" className={styles.btn} />
+      <div className={styles["form-wrapper"]}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          required
+          className={styles["form-control"]}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+          className={styles["form-control"]}
+        />
+        <input type="submit" value="Login" className={styles.btn} />
+        <hr />
+        <div className={styles["signup-wrapper"]}>
+          <Link href="" className={styles["signup-link"]}>
+            Not registered? Click to signup
+          </Link>
+        </div>
+      </div>
     </form>
   );
 }
