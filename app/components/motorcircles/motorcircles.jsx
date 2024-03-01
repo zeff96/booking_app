ddcae41bb0431cd62279a7303018f04698e7c236
zeff96 <zeffo1975@gmail.com>
2024-03-01 13:50:38 +0300

    Add render for motorcircles on the page

--- app/components/motorcircles/motorcircles.jsx
+++ app/components/motorcircles/motorcircles.jsx
@@ -8,10 +8,17 @@ export async function MotorCircles() {
   if (session) {
     let token = session.user.accessToken;
     let motorcircles = await getMotorcircles(token);
-    console.log(motorcircles);
+    let listMotorcircles = motorcircles.map((motor) => (
+      <li key={motor.id}>
+        <h2>{motor.name}</h2>
+        <img src={motor.image} alt={motor.name} />
+        <p>{motor.bio}</p>
+      </li>
+    ));
     return (
       <div>
         <h2>Select a motorcircle</h2>
+        {<ul>{listMotorcircles}</ul>}
       </div>
     );
   }
