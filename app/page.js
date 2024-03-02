4f4eea5f2aa88ad61f51c0ce53dd0717c65424d8
zeff96 <zeffo1975@gmail.com>
2024-03-02 14:17:06 +0300

    Add login page invocation to app home page

--- app/page.js
+++ app/page.js
@@ -1,15 +1,5 @@
-import styles from "./page.module.css";
 import LoginPage from "./login/login";
-import { getServerSession } from "next-auth";
-import { MotorCircles } from "./components/motorcircles/motorcircles";
-import { options } from "./api/auth/[...nextauth]/options";
 
 export default async function Home() {
-  const session = await getServerSession(options);
-
-  return (
-    <main className={styles.main} id="main">
-      {session ? <MotorCircles /> : <LoginPage />}
-    </main>
-  );
+  return <LoginPage />;
 }
