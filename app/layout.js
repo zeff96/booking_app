86b70277078a8f28a515edd49f82ea5bf8cc4cd0
zeff96 <zeffo1975@gmail.com>
2024-03-02 14:12:42 +0300

    Remove navbar from root layout to custom layout

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/navbar/navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,10 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Navbar />
-        <main className="main">{children}</main>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
