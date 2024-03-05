106c605e93332356d8a523ba71cb8e6f562bb587
zeff96 <zeffo1975@gmail.com>
2024-03-05 22:27:53 +0300

    Add register page component

---
+++ app/register/register.jsx
@@ -0,0 +1,43 @@
+export default function RegisterPage() {
+  return (
+    <form className={styles.form}>
+      <div className={styles["form-wrapper"]}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Name"
+          required
+          className={styles["form-control"]}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          required
+          className={styles["form-control"]}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+          className={styles["form-control"]}
+        />
+        <input
+          type="password"
+          name="password_confirmation"
+          placeholder="Password confirmation"
+          required
+          className={styles["form-control"]}
+        />
+        <input type="submit" value="Login" className={styles.btn} />
+        <hr />
+        <div className={styles["login-wrapper"]}>
+          <Link href="" className={styles["signup-link"]}>
+            Registered? Click to login
+          </Link>
+        </div>
+      </div>
+    </form>
+  );
+}
