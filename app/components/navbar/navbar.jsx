24cf10b3ea182e7ceaf0b9a2e287c64927849a90
zeff96 <zeffo1975@gmail.com>
2024-03-06 21:59:57 +0300

    Add logo

--- app/components/navbar/navbar.jsx
+++ app/components/navbar/navbar.jsx
@@ -10,6 +10,11 @@ const Navbar = async () => {
 
   return (
     <nav className={styles.navbar} id="nav">
+      <img
+        src="https://i.pinimg.com/originals/4f/8f/6e/4f8f6eb8e47d42930a4fea87a575d653.jpg"
+        alt="logo"
+        style={{ width: "100px", height: "50px" }}
+      />
       <SideItems />
       {session && <LogoutButton />}
     </nav>
