b6fdab9075497e3944810e323a439cff30d399c2
zeff96 <zeffo1975@gmail.com>
2024-03-15 08:55:43 +0300

    Add login page component

---
+++ app/auth/login/login.jsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+import styles from "./login.module.css";
+import { useRouter } from "next/navigation";
+
+export default function LoginPage() {
+  let router = useRouter();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    let formData = new FormData(event.currentTarget);
+
+    await signIn("credentials", {
+      email: formData.get("email"),
+      password: formData.get("password"),
+      redirect: false,
+    });
+
+    router.push("/motorcircles");
+  }
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <div className={styles["form-wrapper"]}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          required
+          className={styles["form-control"]}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+          className={styles["form-control"]}
+        />
+        <input type="submit" value="Login" className={styles.btn} />
+      </div>
+    </form>
+  );
+}
