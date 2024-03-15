3b246f68b57c417a6750a692b4f4df47f5df8222
zeff96 <zeffo1975@gmail.com>
2024-03-15 08:54:38 +0300

    Add registration page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,24 @@
-import LoginPage from "./login/login";
+import LoginPage from "./auth/login/login";
+import Register from "./auth/Register/Register";
+import styles from "./page.module.css";
 
 export default async function Home() {
-  return <LoginPage />;
+  return (
+    <>
+      <LoginPage />
+      <hr />
+      <div className={styles["signup-wrapper"]}>
+        <button type="button" className={styles["signup-link"]}>
+          Not registered? Click to signup
+        </button>
+        <div className={styles.modal}>
+          <div className={styles["modal-content"]}></div>
+          <div className={styles["modal-header"]}></div>
+          <div className={styles["modal-body"]}>
+            <Register />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
