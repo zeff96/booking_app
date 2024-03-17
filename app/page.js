c860d9aee86ce4b91ed020d8e81748b6e49b761d
zeff96 <zeffo1975@gmail.com>
2024-03-17 21:23:46 +0300

    Add wrapper around  main elements

--- app/page.js
+++ app/page.js
@@ -4,21 +4,23 @@ import styles from "./page.module.css";
 
 export default async function Home() {
   return (
-    <>
-      <LoginPage />
-      <hr />
-      <div className={styles["signup-wrapper"]}>
-        <button type="button" className={styles["signup-link"]}>
-          Not registered? Click to signup
-        </button>
-        <div className={styles.modal}>
-          <div className={styles["modal-content"]}></div>
-          <div className={styles["modal-header"]}></div>
-          <div className={styles["modal-body"]}>
-            <Register />
+    <main className={styles.main}>
+      <div className={styles["main-wrapper"]}>
+        <LoginPage />
+        <hr />
+        <div className={styles["signup-wrapper"]}>
+          <button type="button" className={styles["signup-link"]}>
+            Not registered? Click to signup
+          </button>
+          <div className={styles.modal}>
+            <div className={styles["modal-content"]}></div>
+            <div className={styles["modal-header"]}></div>
+            <div className={styles["modal-body"]}>
+              <Register />
+            </div>
           </div>
         </div>
       </div>
-    </>
+    </main>
   );
 }
