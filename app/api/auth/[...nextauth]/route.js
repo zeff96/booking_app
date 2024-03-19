78eaa47400c7bba425971e6097ffa906d539b186
zeff96 <zeffo1975@gmail.com>
2024-03-19 22:15:01 +0300

    Remove Next auth from the project

--- app/api/auth/[...nextauth]/route.js
+++
@@ -1,6 +0,0 @@
-import nextAuth from "next-auth";
-import { options } from "./options";
-
-let handler = nextAuth(options);
-
-export { handler as GET, handler as POST };
--- app/components/session/SessionProvider.js
+++
@@ -1,7 +0,0 @@
-import { SessionProvider } from "next-auth/react";
-
-const SessionProviders = ({ children }) => {
-  return <SessionProvider>{children}</SessionProvider>;
-};
-
-export default SessionProviders;
