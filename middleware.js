07b4348f274cd3feb94c620519206c23cfccd784
zeff96 <zeffo1975@gmail.com>
2024-03-20 10:00:01 +0300

    Add middleware to protect pages

--- middleware.js
+++ middleware.js
@@ -1,5 +1,15 @@
-export { default } from "next-auth/middleware";
+export const middleware = (request) => {
+  const token = request.cookies.get("token")?.value;
+
+  if (token && !request.nextUrl.pathname.startsWith("/motorcircles")) {
+    return Response.redirect(new URL("/motorcircles", request.url));
+  }
+
+  if (!token && !request.nextUrl.pathname.startsWith("/")) {
+    return Response.redirect(new URL("/", request.url));
+  }
+};
 
 export const config = {
-  matcher: ["/motorcircles/:path*"],
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
 };
