ae445e4763687df3fc8d85284c1d8e4493ca6697
Adeka Zeff's <zeffo1975@gmail.com>
2024-03-27 01:40:50 +0300

    Update to server component
    Remove client definition and hooks from the page and make it a server component

--- app/page.js
+++ app/page.js
@@ -1,12 +1,8 @@
-"use client";
-
-import { useState } from "react";
 import LoginPage from "./auth/login/login";
 import Register from "./auth/Register/Register";
 import styles from "./page.module.css";
 
 export default async function Home() {
-  const [show, setShow] = useState(false);
   return (
     <main className={styles.main}>
       <div className={styles["main-wrapper"]}>
@@ -16,11 +12,7 @@ export default async function Home() {
           <button type="button" className={styles["signup-link"]}>
             Not registered? Click to signup
           </button>
-          <div
-            className={
-              show ? [styles.modal, styles.show].join(" ") : styles.modal
-            }
-          >
+          <div>
             <div className={styles["modal-content"]}></div>
             <div className={styles["modal-header"]}></div>
             <div className={styles["modal-body"]}>
