352f64532e10439e5242dfff52a7e164d63d77b2
zeff96 <zeffo1975@gmail.com>
2024-04-21 09:10:39 +0300

    Add middleware to contol flow of user access control

--- middleware.js
+++ middleware.js
@@ -1,13 +1,24 @@
+import { publicRoutes, authRoutes, DEFAULT_REDIRECT_ROUTE } from "./routes";
+
 export const middleware = (request) => {
+  const { nextUrl } = request;
   const currentUser = request.cookies.get("token")?.value;
 
-  if (currentUser && !request.nextUrl.pathname.startsWith("/motorcircles")) {
-    return Response.redirect(new URL("/motorcircles", request.url));
+  const isAuthRoutes = authRoutes.includes(nextUrl.pathname);
+  const isPublicRoutes = publicRoutes.includes(nextUrl.pathname);
+
+  if (isAuthRoutes) {
+    if (currentUser) {
+      return Response.redirect(new URL(DEFAULT_REDIRECT_ROUTE, nextUrl));
+    }
+    return null;
   }
 
-  if (!currentUser && !request.nextUrl.pathname.startsWith("/")) {
-    return Response.redirect(new URL("/", request.url));
+  if (!currentUser && !isPublicRoutes) {
+    return Response.redirect(new URL("/auth/login", nextUrl));
   }
+
+  return null;
 };
 
 export const config = {
