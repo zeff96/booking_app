adb6c1a29f62b723329925ed537a3bf8e268951c
zeff96 <zeffo1975@gmail.com>
2025-01-09 00:46:17 +0300

    Add login app router for login page

---
+++ app/login/page.js
@@ -0,0 +1,8 @@
+import LoginForm from "@/app/ui/auth/login";
+export default function LoginPage() {
+  return (
+    <main className="w-full h-screen flex flex-col lg:items-center justify-center bg-slate-400 p-3">
+      <LoginForm />
+    </main>
+  );
+}
