bf2efa7e4f4f5a8900966c422d19ce847c9a5c84
zeff96 <zeffo1975@gmail.com>
2025-01-09 00:45:28 +0300

    Add the main application homepage for and enable links for login in

--- app/page.js
+++ app/page.js
@@ -1,12 +1,33 @@
-import styles from "./page.module.css";
-import { SigninWrapperButton } from "./components/button/signin-wrapper";
+import Link from "next/link";
+import Image from "next/image";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import { lusitana } from "@/app/ui/fonts";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <SigninWrapperButton>
-        <button type="button">Signin</button>
-      </SigninWrapperButton>
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+          <p className={`${lusitana.className} antialiased`}>
+            <span className="font-semibold">Welcome to Vespa.</span> This is
+            online booking app for vespas motorcycles
+          </p>
+          <Link
+            href="/login"
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+          >
+            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+          </Link>
+        </div>
+        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+          <Image
+            src="/vespa.jpg"
+            alt="desktop image"
+            width={500}
+            height={500}
+          />
+        </div>
+      </div>
     </main>
   );
 }
