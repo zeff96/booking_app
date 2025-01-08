e8418609faed097b76768e5b6df7a5fda6e58788
zeff96 <zeffo1975@gmail.com>
2025-01-09 00:47:50 +0300

    Add app directory for user registration

---
+++ app/register/page.js
@@ -0,0 +1,8 @@
+import RegistrationForm from "@/app/ui/auth/register";
+export default function LoginPage() {
+  return (
+    <main className="w-full lg:items-center h-screen flex flex-col justify-center p-3 bg-slate-400">
+      <RegistrationForm />
+    </main>
+  );
+}
