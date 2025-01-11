225e12b3438ecd97360fa59ec9169c643ba37b25
zeff96 <zeffo1975@gmail.com>
2025-01-11 03:55:20 +0300

    Update signupform  component name

--- app/register/page.js
+++ app/register/page.js
@@ -1,8 +1,8 @@
-import RegistrationForm from "@/app/ui/auth/register";
+import SignupForm from "@/app/ui/auth/sign-up-from";
 export default function LoginPage() {
   return (
     <main className="w-full lg:items-center h-screen flex flex-col justify-center p-3 bg-slate-400">
-      <RegistrationForm />
+      <SignupForm />
     </main>
   );
 }
