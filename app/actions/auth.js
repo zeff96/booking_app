dd0fd6fb02e87243793c13edf844ee4c5317a2ec
zeff96 <zeffo1975@gmail.com>
2025-01-12 20:30:22 +0300

    Add action to sign in registered users

--- app/actions/auth.js
+++ app/actions/auth.js
@@ -1,6 +1,8 @@
 "use server";
 
-import { SignupFormSchema } from "@/app/lib/definitions";
+import { SigninFormSchema, SignupFormSchema } from "@/app/lib/definitions";
+import { createSession, deleteSession } from "@/app/lib/session";
+import { redirect } from "next/navigation";
 
 export const signup = async (state, formData) => {
   const validatedFields = SignupFormSchema.safeParse({
@@ -38,11 +40,61 @@ export const signup = async (state, formData) => {
 
   const data = await response.json();
 
-  console.log(data);
+  const { user } = data;
 
-  // if (!user) {
-  //   return {
-  //     message: "An error occurred while creating your account.",
-  //   };
-  // }
+  if (data.error) {
+    return {
+      message: data.error,
+    };
+  }
+
+  await createSession(user.id);
+  redirect("/motorcycles");
+};
+
+export const signin = async (state, formData) => {
+  const validatedFields = SigninFormSchema.safeParse({
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+
+  const { email, password } = validatedFields.data;
+
+  const userData = {
+    user: {
+      email,
+      password,
+    },
+  };
+
+  const response = await fetch("http://127.0.0.1:3000/auth/sign_in", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userData),
+  });
+
+  const data = await response.json();
+  const { user } = data;
+
+  if (data.error) {
+    return {
+      message: data.error,
+    };
+  }
+
+  createSession(user.id);
+  redirect("/motorcycles");
+};
+
+export const logout = async () => {
+  deleteSession();
+  redirect("/login");
 };
