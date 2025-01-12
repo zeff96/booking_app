3e9e36e541223b8536d9462899ed00e3c8ef6cb5
zeff96 <zeffo1975@gmail.com>
2025-01-12 20:31:05 +0300

    Add methods to encrypt, decrypt and create session

---
+++ app/lib/session.js
@@ -0,0 +1,42 @@
+import "server-only";
+import { SignJWT, jwtVerify } from "jose";
+import { cookies } from "next/headers";
+
+const secretKey = process.env.SESSION_SECRET;
+const encodedKey = new TextEncoder().encode(secretKey);
+
+export const encrypt = async (payload) => {
+  return new SignJWT(payload)
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setExpirationTime("7d")
+    .sign(encodedKey);
+};
+
+export const decrypt = async (session) => {
+  try {
+    const { payload } = await jwtVerify(session, encodedKey, {
+      algorithms: ["HS256"],
+    });
+    return payload;
+  } catch (error) {
+    console.log("Failed to verify session");
+  }
+};
+
+export const createSession = async (userId) => {
+  const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+  const session = await encrypt({ userId, expiresAt });
+  const cookieStore = await cookies();
+
+  cookieStore.set("session", session, {
+    httpOnly: true,
+    expires: expiresAt,
+    path: "/",
+  });
+};
+
+export const deleteSession = async () => {
+  const cookieStore = await cookies();
+  cookieStore.delete("session");
+};
