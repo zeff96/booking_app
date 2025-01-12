1b7dfc69dc441704dd0b67de381dbfe25a432f23
zeff96 <zeffo1975@gmail.com>
2025-01-12 20:31:31 +0300

    Add update for sign in form relative path

--- app/login/page.js
+++ app/login/page.js
@@ -1,4 +1,4 @@
-import LoginForm from "@/app/ui/auth/login";
+import LoginForm from "@/app/ui/auth/sign-in-form";
 export default function LoginPage() {
   return (
     <main className="w-full h-screen flex flex-col lg:items-center justify-center bg-slate-400 p-3">
