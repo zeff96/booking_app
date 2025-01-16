03ab4b6f7c9cf4489050e95634fc2cd8ea96e916
zeff96 <zeffo1975@gmail.com>
2025-01-16 06:18:17 +0300

    Add render for motorcycles on the page

---
+++ app/motorcycles/(overview)/page.js
@@ -0,0 +1,15 @@
+import { Motorcycles } from "@/app/ui/motorcycles/motorcycles";
+
+export default async function Page() {
+  return (
+    <main className="w-full h-screen flex flex-col justify-center">
+      <div className="text-center mb-6">
+        <h2 className="font-bold uppercase text-2xl">Latest Models</h2>
+        <span className="text-sm text-gray-300">
+          Please select a Vespa Model
+        </span>
+      </div>
+      <Motorcycles />
+    </main>
+  );
+}
