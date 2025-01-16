172950b41778a00f9c603c7df14650f74b406076
zeff96 <zeffo1975@gmail.com>
2025-01-16 06:20:31 +0300

    Update sign up form path in the register page

--- app/register/page.js
+++ app/register/page.js
@@ -1,5 +1,5 @@
-import SignupForm from "@/app/ui/auth/sign-up-from";
-export default function LoginPage() {
+import SignupForm from "@/app/ui/auth/sign-up-form";
+export default function Page() {
   return (
     <main className="w-full lg:items-center h-screen flex flex-col justify-center p-3 bg-slate-400">
       <SignupForm />
