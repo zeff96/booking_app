798ec60382cd92740ee32e3860518e4faa669657
zeff96 <zeffo1975@gmail.com>
2025-01-16 06:15:53 +0300

    Add a method to formatCurrency into string

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -4,3 +4,10 @@ import clsx from "clsx";
 export const cssMerge = (...inputs) => {
   return twMerge(clsx(inputs));
 };
+
+export const formatCurrency = (amount) => {
+  return (amount / 100).toLocaleString("en-EU", {
+    style: "currency",
+    currency: "EUR",
+  });
+};
