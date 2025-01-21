4ad384fc48846269bc215768604c997d42552982
zeff96 <zeffo1975@gmail.com>
2025-01-21 12:08:30 +0300

    Add project update to motorcycles ui component

--- app/ui/motorcycles/motorcycles.js
+++ app/ui/motorcycles/motorcycles.js
@@ -7,6 +7,13 @@ import {
   CardTitle,
 } from "../card/card";
 import { motorcycles } from "@/app/lib/data";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 import { CiFacebook, CiInstagram } from "react-icons/ci";
 import { TiSocialTwitterCircular } from "react-icons/ti";
 import Image from "next/image";
@@ -20,42 +27,55 @@ const imageStyle = {
 
 export async function Motorcycles() {
   return (
-    <div className="grid grid-cols-1 xl:grid-cols-2 justify-items-center overflow-y-scroll">
-      {motorcycles.map((item) => (
-        <Card
-          key={item.id}
-          className="w-[250px] md:w-[500px] py-6 mb-3 flex flex-col items-center"
-        >
-          <CardHeader className="relative w-[200px] h-[200px]">
-            <Image
-              src={item.image}
-              alt={item.name}
-              style={imageStyle}
-              fill={true}
-            />
-          </CardHeader>
-          <CardContent>
-            <CardTitle>
-              <Link
-                href={`motorcycles/${item.id}`}
-                className="hover:opacity-75"
-              >
-                {item.name}
-              </Link>
-            </CardTitle>
-            <CardDescription>
-              {item.bio.length >= 25
-                ? `${item.bio.substring(0, 100)}...`
-                : item.bio}
-            </CardDescription>
-          </CardContent>
-          <CardFooter className="justify-center gap-2">
-            <CiFacebook />
-            <TiSocialTwitterCircular />
-            <CiInstagram />
-          </CardFooter>
-        </Card>
-      ))}
-    </div>
+    <Carousel
+      opts={{
+        align: "start",
+      }}
+      className="w-full"
+    >
+      <CarouselContent className="-ml-1">
+        {motorcycles.map((item) => (
+          <CarouselItem
+            key={item.id}
+            className="pl-1 md:basis-1/2 lg:basis-1/3"
+          >
+            <div className="p-1">
+              <Card className="py-6 mb-3 flex flex-col items-center">
+                <CardHeader className="relative w-[200px] h-[200px]">
+                  <Image
+                    src={item.image}
+                    alt={item.name}
+                    style={imageStyle}
+                    fill={true}
+                  />
+                </CardHeader>
+                <CardContent className="text-center">
+                  <CardTitle className="mb-3">
+                    <Link
+                      href={`motorcycles/${item.id}`}
+                      className="hover:opacity-75"
+                    >
+                      {item.name}
+                    </Link>
+                  </CardTitle>
+                  <CardDescription className="text-sm text-gray-400">
+                    {item.bio.length >= 25
+                      ? `${item.bio.substring(0, 100)}...`
+                      : item.bio}
+                  </CardDescription>
+                </CardContent>
+                <CardFooter className="justify-center gap-2">
+                  <CiFacebook />
+                  <TiSocialTwitterCircular />
+                  <CiInstagram />
+                </CardFooter>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
   );
 }
