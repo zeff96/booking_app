19f1036413466ef8e610e26b5a10a2818eaaf72c
zeff96 <zeffo1975@gmail.com>
2025-01-21 12:07:29 +0300

    Update motorcycle page

--- app/motorcycles/layout.js
+++ app/motorcycles/layout.js
@@ -14,10 +14,9 @@ export default function MotorcycleLayout({ children }) {
           <Image src="/icon.jpg" alt="navbar icon" width={150} height={150} />
         </Link>
         <Menu />
-        <div className="mt-auto flex justify-center lg:justify-start">
+        <div className="mt-auto flex justify-center lg:justify-start pb-6">
           <LogoutButton />
         </div>
-        <form action="" method="post"></form>
       </div>
       <div className="w-[86%] md:w-[92%] lg:w-[75%] xl:w-[80%] px-3 py-3">
         {children}
