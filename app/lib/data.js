19a2531ad4e6531087d6c5cd109cad812eecf371
zeff96 <zeffo1975@gmail.com>
2025-01-27 22:45:55 +0300

    Update images location to make them static images

--- app/lib/data.js
+++ app/lib/data.js
@@ -1,3 +1,8 @@
+import vespa1 from "@/app/images/vespa1.avif";
+import vespa2 from "@/app/images/vespa2.jpg";
+import vespa3 from "@/app/images/vespa3.webp";
+import vespa4 from "@/app/images/vespa4.jpg";
+
 export const user = {
   id: "ecf5259f-aa69-4a26-8ac2-2819209ddb15",
   username: "user",
@@ -20,7 +25,7 @@ export const motorcycles = [
     user_id: "ecf5259f-aa69-4a26-8ac2-2819209ddb15",
     name: "Vespa Bello",
     bio: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-    image: "/vespa1.avif",
+    image: vespa1,
     financeFee: 10000,
     optionToPurchase: 15000,
     totalAmountPayable: 30000,
@@ -31,7 +36,7 @@ export const motorcycles = [
     user_id: "ecf5259f-aa69-4a26-8ac2-2819209ddb15",
     name: "Vespa Hero",
     bio: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-    image: "/vespa2.jpg",
+    image: vespa2,
     financeFee: 15050,
     optionToPurchase: 20000,
     totalAmountPayable: 35000,
@@ -42,7 +47,7 @@ export const motorcycles = [
     user_id: "4497ac79-a608-4655-b530-101c9af965e7",
     name: "Vespa Primera",
     bio: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-    image: "/vespa3.webp",
+    image: vespa3,
     financeFee: 10500,
     optionToPurchase: 15000,
     totalAmountPayable: 20000,
@@ -53,7 +58,7 @@ export const motorcycles = [
     user_id: "4497ac79-a608-4655-b530-101c9af965e7",
     name: "Vespa Scotter",
     bio: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-    image: "/vespa4.jpg",
+    image: vespa4,
     financeFee: 20000,
     optionToPurchase: 40000,
     totalAmountPayable: 45000,
