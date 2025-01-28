d07058741d507ad09631a6e4e9a774509089260b
zeff96 <zeffo1975@gmail.com>
2025-01-28 23:51:12 +0300

    Add link to reserve an vespa from vespa details page

--- app/motorcycles/reserve/page.js
+++ app/motorcycles/reserve/page.js
@@ -1,6 +1,11 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
 import React from "react";
 
 const Reserve = () => {
+  const searchParams = useSearchParams();
+  const motorcycleId = searchParams.get("motorcycleId");
   return (
     <div>
       <h2>Add reservation</h2>
