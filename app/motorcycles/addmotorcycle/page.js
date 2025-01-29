140bda11bd0b90983ef1adefb2805ce2e41ba657
zeff96 <zeffo1975@gmail.com>
2025-01-29 23:52:59 +0300

    Add router page for add new vespa

---
+++ app/motorcycles/addmotorcycle/page.js
@@ -0,0 +1,10 @@
+import { AddMotorcycleForm } from "@/app/ui/motorcycles/addmotorcycle/add-motorcycle";
+
+export default function Page() {
+  return (
+    <div>
+      <h2 className="text-center font-semibold text-2xl mb-3">Add Vespa</h2>
+      <AddMotorcycleForm />
+    </div>
+  );
+}
