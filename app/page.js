f25818ff57ed07014ea234912656dc25bfacc032
zeff96 <zeffo1975@gmail.com>
2025-01-31 15:02:51 +0300

    Update path to user login

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
             online booking app for vespas motorcycles
           </p>
           <Link
-            href="/login"
+            href="/auth/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
