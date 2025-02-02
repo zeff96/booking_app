60b65d31acc356f4a66a7c6b40f061521138e457
zeff96 <zeffo1975@gmail.com>
2025-02-02 23:48:22 +0300

    Add streaming for motorcycles from the api

---
+++ app/motorcycles/page.js
@@ -0,0 +1,20 @@
+import { Motorcycles } from "@/app/ui/motorcycles/motorcycles";
+import { CardWrapperSkeleton } from "@/app/ui/motorcycles/skeletons";
+import { Suspense } from "react";
+
+export default async function Page() {
+  return (
+    <main className="w-full h-screen flex flex-col justify-center px-12">
+      <div className="text-center mb-6">
+        <h2 className="font-bold uppercase text-2xl">Latest Models</h2>
+        <span className="text-sm text-gray-300">
+          Please select a Vespa Model
+        </span>
+      </div>
+
+      <Suspense fallback={<CardWrapperSkeleton />}>
+        <Motorcycles />
+      </Suspense>
+    </main>
+  );
+}
