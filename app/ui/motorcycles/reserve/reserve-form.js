754164f5d4ff19c7d2b2995531d40ad82f610f20
zeff96 <zeffo1975@gmail.com>
2025-02-06 18:55:25 +0300

    Update reserve form to load list of cities and motorcycles

--- app/ui/motorcycles/reserve/reserve-form.js
+++ app/ui/motorcycles/reserve/reserve-form.js
@@ -1,40 +1,31 @@
-export function ReserveForm({ username, motorcycleId }) {
+import { reserveMotorcycle } from "@/app/actions/action";
+
+export function ReserveForm({ userId, motorcycleId, cities, motorcycles }) {
+  const reservationWithUser = reserveMotorcycle.bind(null, userId);
+
   return (
-    <form>
+    <form action={reservationWithUser}>
       <div className="mb-3">
-        <label htmlFor="username" className="sr-only">
-          Username
-        </label>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          readOnly
-          value={username}
-          className="appearance-none block w-full p-3 border border-gray-300 rounded-md"
-        />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="motorcycleId" className="sr-only">
+        <label htmlFor="motorcycle" className="sr-only">
           motorcycle
         </label>
         {motorcycleId ? (
-          <input
-            type="text"
-            id="motorcycleId"
-            name="motorcycleId"
-            readOnly
-            value={motorcycleId}
-            className="appearance-none block w-full p-3 border border-gray-300 rounded-md"
-          />
+          <input type="hidden" name="motorcycle" value={motorcycleId} />
         ) : (
           <select
+            name="motorcycle"
+            id="motorcycle"
             className="appearance-none block w-full p-3 border border-gray-300 rounded-md"
             defaultValue="Select motorcycle..."
           >
             <option disabled defaultValue="Select motorcycle...">
               Select motorcycle...
             </option>
+            {motorcycles.map((motorcycle) => (
+              <option key={motorcycle.id} value={motorcycle.id}>
+                {motorcycle.name}
+              </option>
+            ))}
           </select>
         )}
       </div>
@@ -63,6 +54,11 @@ export function ReserveForm({ username, motorcycleId }) {
           <option disabled defaultValue="Select city...">
             Select city...
           </option>
+          {cities.map((city) => (
+            <option key={city.id} value={city.name}>
+              {city.name}
+            </option>
+          ))}
         </select>
       </div>
       <div className="grid">
