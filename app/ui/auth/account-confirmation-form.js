c8d08539d91137d261fc877b112a38c9b07f32a7
zeff96 <zeffo1975@gmail.com>
2025-02-06 18:53:57 +0300

    Add disabled state while the form is submitting

--- app/ui/auth/account-confirmation-form.js
+++ app/ui/auth/account-confirmation-form.js
@@ -1,21 +1,22 @@
 "use client";
 
 import { verifyAccount } from "@/app/actions/auth";
-import { useActionState } from "react";
 
 export const AccountConfirmationForm = ({ token }) => {
-  const [state, action, pending] = useActionState(verifyAccount, undefined);
+  const verifyAccountWithToken = verifyAccount.bind(null, token);
+
   return (
-    <form action={action}>
-      <input type="hidden" name="token" value={token} />
+    <form action={verifyAccountWithToken}>
       <div>
         <button
+          disabled={pending}
           type="submit"
           className="bg-blue-500 text-white p-3 rounded-md hover:bg-blue-700"
         >
           Verify email
         </button>
       </div>
+      {state?.message && <div className="text-red-500">{state.message}</div>}
     </form>
   );
 };
