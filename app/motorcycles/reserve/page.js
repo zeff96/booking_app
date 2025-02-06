01e0a1fe2c370eb6f6588ad6d98e9b0d8451faf8
zeff96 <zeffo1975@gmail.com>
2025-02-06 18:52:54 +0300

    Add render for reservation form on the page

--- app/motorcycles/reserve/page.js
+++ app/motorcycles/reserve/page.js
@@ -1,16 +1,23 @@
-"use client";
-
+import { fetchCities } from "@/app/actions/action";
+import getMotorcycles from "@/app/actions/motorcycles/motorcles";
+import { verifySession } from "@/app/lib/dal";
 import { ReserveForm } from "@/app/ui/motorcycles/reserve/reserve-form";
-import { useSearchParams } from "next/navigation";
-import React from "react";
 
-const Reserve = () => {
-  const searchParams = useSearchParams();
-  const motorcycleId = searchParams.get("motorcycleId");
+const Reserve = async ({ searchParams }) => {
+  const motorcycleId = (await searchParams).motorcycleId;
+  const { userId } = await verifySession();
+  const cities = await fetchCities();
+  const motorcycles = await getMotorcycles();
+
   return (
     <div className="w-full h-screen flex flex-col gap-y-4">
       <h2 className="text-center text-2xl font-semibold">Add reservation</h2>
-      <ReserveForm username="john" motorcycleId={motorcycleId} />
+      <ReserveForm
+        userId={userId}
+        motorcycleId={motorcycleId}
+        cities={cities}
+        motorcycles={motorcycles}
+      />
     </div>
   );
 };
