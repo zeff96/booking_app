55df14269388075950fc3f7313c4f5f1de345ece
zeff96 <zeffo1975@gmail.com>
2025-02-06 18:50:27 +0300

    Add more css styles to the motorcycles homepage

--- app/motorcycles/page.js
+++ app/motorcycles/page.js
@@ -5,9 +5,9 @@ import { Suspense } from "react";
 export default async function Page() {
   return (
     <main className="w-full h-screen flex flex-col justify-center px-12">
-      <div className="text-center mb-6">
+      <div className="text-center mb-12">
         <h2 className="font-bold uppercase text-2xl">Latest Models</h2>
-        <span className="text-sm text-gray-300">
+        <span className="text-sm text-gray-300 border-b-2 border-dashed border-gray-300 pb-6">
           Please select a Vespa Model
         </span>
       </div>
