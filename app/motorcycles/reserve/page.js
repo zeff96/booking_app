58bd2da1de9d530cbca8fb1f5b1b50c820d4a136
zeff96 <zeffo1975@gmail.com>
2025-02-07 19:49:33 +0300

    Remove user id passed as prop to the form

--- app/motorcycles/reserve/page.js
+++ app/motorcycles/reserve/page.js
@@ -1,11 +1,9 @@
 import { fetchCities } from "@/app/actions/action";
 import getMotorcycles from "@/app/actions/motorcycles/motorcles";
-import { verifySession } from "@/app/lib/dal";
 import { ReserveForm } from "@/app/ui/motorcycles/reserve/reserve-form";
 
 const Reserve = async ({ searchParams }) => {
   const motorcycleId = (await searchParams).motorcycleId;
-  const { userId } = await verifySession();
   const cities = await fetchCities();
   const motorcycles = await getMotorcycles();
 
@@ -13,7 +11,6 @@ const Reserve = async ({ searchParams }) => {
     <div className="w-full h-screen flex flex-col gap-y-4">
       <h2 className="text-center text-2xl font-semibold">Add reservation</h2>
       <ReserveForm
-        userId={userId}
         motorcycleId={motorcycleId}
         cities={cities}
         motorcycles={motorcycles}
